1cb1e97c24df7f85a25488790646f4e15edbe123
Mohammad Reza <reza.najibf@gmail.com>
2022-08-07 21:58:29 +0430

    fix: nativewind stylesheet output removed

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '../components/Themed';
 export default function TabTwoScreen() {
   return (
     <View className='flex-1 h-full items-center justify-center'>
-      <Text className='text-xl font-bold'>Tab Two</Text>
+      <Text className='text-xl font-bold'>Tab 2</Text>
     </View>
   );
 }
